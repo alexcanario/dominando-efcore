98958708218df45d20575734b57e2d09ded9a1e0
Alex Canario <alexcanario@gmail.com>
2023-01-19 19:19:32 -0300

    Aula 7.7.04 Gravando seus logs em um arquivo

--- projeto1/Data/ApplicationContext.cs
+++ projeto1/Data/ApplicationContext.cs
@@ -7,6 +7,7 @@ namespace projeto1.Data
 {
     public class ApplicationContext : DbContext
     {
+        private readonly StreamWriter _writer = new StreamWriter("ef-core-log-txt", append: true);
         public DbSet<Funcionario>? Funcionarios { get; set; }
         public DbSet<Departamento>? Departamentos { get; set; }
 
@@ -15,11 +16,18 @@ namespace projeto1.Data
             const string strConn = "Server=(local); Database=C003; Encrypt=True; Integrated Security=True; Trust Server Certificate=true; pooling=true; MultipleActiveResultSets=True";
             optionsBuilder
                 .UseSqlServer(strConn)
-                .LogTo(Console.WriteLine,
-                    new[] { CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted },
-                    LogLevel.Information,
-                    DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine);
+                // .LogTo(Console.WriteLine,
+                //     new[] { CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted },
+                //     LogLevel.Information,
+                //     DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine);
+                .LogTo(_writer.WriteLine, LogLevel.Information);
 
+        }
+
+        public override void Dispose()
+        {
+            _writer.Dispose();
+            base.Dispose();
         }
     }
 }
