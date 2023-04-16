3ac6b895cbb706f5fa7ed85f5633cbf9b41a0094
Alex Canario <alexcanario@gmail.com>
2023-04-16 19:10:49 -0300

    Aula 8.14, Custumizando muitos para muitos

--- projeto1/Config/AtorFilmeConfig.cs
+++ projeto1/Config/AtorFilmeConfig.cs
@@ -8,9 +8,22 @@ public class AtorFilmeConfig : IEntityTypeConfiguration<Ator>
 {
     public void Configure(EntityTypeBuilder<Ator> builder)
     {
+        // builder
+        //     .HasMany(a => a.Filmes)
+        //     .WithMany(f => f.Atores)
+        //     .UsingEntity(j => j.ToTable("AtorXFilme"));
+
         builder
-            .HasMany(a => a.Filmes)
-            .WithMany(f => f.Atores)
-            .UsingEntity(j => j.ToTable("AtorXFilme"));
+        .HasMany(a => a.Filmes)
+        .WithMany(f => f.Atores)
+        .UsingEntity<Dictionary<string, object>>(
+            "FilmesAtores",
+            l => l.HasOne<Filme>().WithMany().HasForeignKey("FilmeId"),
+            r => r.HasOne<Ator>().WithMany().HasForeignKey("AtorId"),
+            j =>
+            {
+                j.Property<DateTime>("CadastradoEm").HasDefaultValueSql("GETDATE()");
+            }
+        );
     }
 }
